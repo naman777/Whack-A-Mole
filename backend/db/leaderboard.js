f58c959c9d15b5a09fae82a3d6bc1ed91946642c
Aryan Singh <aryansingh.myname@gmail.com>
2024-07-30 22:44:27 +0530

    added mongoose schemas

--- backend/db/leaderboard.js
+++ backend/db/leaderboard.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const leaderboardSchema = mongoose.Schema({
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'user',
+        ref: 'User',
         required: true
     },
     score: {
